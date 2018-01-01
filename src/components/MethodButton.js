16f7cbe2ae17c3ab396d60cbcba7ece9985036e3
Nik <NikBaltatzis@gmail.com>
2018-01-01 12:57:00 -0500

    Working on linking up buttons with Snapshots

--- src/components/MethodButton.js
+++ src/components/MethodButton.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import './MethodButton.css';
 
+// function renderChildren(props) {
+//   return React.Children.map(props.children, child => {
+//     return React.cloneElement(child, {
+
+//     });
+//   })
+// }
+function onClick(children) {
+  if (!children) {
+    console.log('no children');
+  } else { 
+    console.log(children);
+  }
+  // if (children.length === 1) {
+  //   console.log(children[0]);
+  // } else if (children.length === 3) {
+  //   console.log(`Child 1: ${children[0]}`);
+  //   console.log(`Child 2: ${children[2]}`);
+  // }
+}
+
 function MethodButton(props) {
   const classNames = ["MethodButton", `MethodButton--${props.methodName}`];
 
@@ -8,7 +29,7 @@ function MethodButton(props) {
     classNames.push("MethodButton--disabled");
   }
   return (
-    <button className={classNames.join(' ')} onClick={props.onClick} disabled={props.disabled}>
+    <button className={classNames.join(' ')} onClick={onClick(props.children)} disabled={props.disabled}>
       {props.methodName}({props.children})
     </button>
   );
