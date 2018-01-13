07206511fa236247ee113fb05779560de122ae79
Nik <NikBaltatzis@gmail.com>
2018-01-13 09:17:57 -0500

    fixed height issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import hideFocusRingExceptForTabbers from './handyScripts/hideFocusRingExceptForTabbers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+document.getElementById('root').style.minHeight = "100vh";
 registerServiceWorker();
 hideFocusRingExceptForTabbers();
