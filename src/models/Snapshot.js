b15c6f184b5884d42fab72bd7da0ae8ce35e98f4
Nik <NikBaltatzis@gmail.com>
2018-01-15 10:06:43 -0500

    Added second set of methods to Snapshot and tests for them

--- src/models/Snapshot.js
+++ src/models/Snapshot.js
@@ -66,21 +66,95 @@ export function set(prevSnapshot, argums) {
 
 export function remove(prevSnapshot, argums) {
   const listValues = prevSnapshot.get('listValues');
-  const index = argums[0];
 
-  if (index < 0 || index >= listValues.size) {
-    throw new Error(`Invalid Index: Tried to remove value at index ${index} but list only contains ${listValues.size} elements`);
+  const argum = argums[0];
+
+  // Need to determine if argument is a value or index
+  if (Number.isInteger(argum)) {
+    const index = argum;
+
+    if (index < 0 || index >= listValues.size) {
+      throw new Error(`Invalid Index: Tried to remove value at index ${index} but list only contains ${listValues.size} elements`);
+    }
+
+    const removedValue = listValues.get(index);
+    const command = Map({ method: 'remove', arguments: argums, returned: removedValue });
+    const newListValues = listValues.delete(index);
+
+    return Snapshot(newListValues, command);
+  } else {
+    const regexp = /^[A-Z]/;
+    if (regexp.test(argum)) {
+      const index = findIndex(listValues, argum);
+
+      if (index === -1) {
+        const command = Map({ method: 'remove', arguments: argums, returned: false });
+        return Snapshot(listValues, command);
+      }
+
+      const newListValues = listValues.delete(index);
+      const command = Map({ method: 'remove', arguments: argums, returned: true });
+      return Snapshot(newListValues, command);
+    } else {
+      throw new Error('Invalid argument passed to remove');
+    }
   }
-
-  const removedValue = listValues.get(index);
-  const command = Map({ method: 'remove', arguments: argums, returned: removedValue });
-  const newListValues = listValues.delete(index);
-
-  return Snapshot(newListValues, command);
 }
 
 export function size(prevSnapshot) {
   const listValues = prevSnapshot.get('listValues');
   const command = Map({ method: 'size', arguments: [], returned: listValues.size });
+  return Snapshot(listValues, command);
+}
+
+export function clear(prevSnapshot) {
+  const command = Map({ method: 'clear', arguments: [], returned: null });
+  return Snapshot(List(), command);
+}
+
+function findIndex(listValues, value) {
+  return listValues.findIndex(valueMap => {
+    return valueMap.get('value') === value;
+  });
+}
+
+export function contains(prevSnapshot, argums) {
+  const listValues = prevSnapshot.get('listValues');
+  const valueToFind = argums[0];
+
+  const found = findIndex(listValues, valueToFind) !== -1;
+
+  const command = Map({ method: 'contains', arguments: argums, returned: found });
+
+  return Snapshot(listValues, command);
+}
+
+export function indexOf(prevSnapshot, argums) {
+  const listValues = prevSnapshot.get('listValues');
+  const valueToFind = argums[0];
+
+  const index = findIndex(listValues, valueToFind);
+
+  const command = Map({ method: 'indexOf', arguments: argums, returned: index });
+
+  return Snapshot(listValues, command);
+}
+
+export function lastIndexOf(prevSnapshot, argums) {
+  const listValues = prevSnapshot.get('listValues');
+  const valueToFind = argums[0];
+
+  const index = listValues.findLastIndex(valueMap => valueMap.get('value') === valueToFind);
+
+  const command = Map({ method: 'lastIndexOf', arguments: argums, returned: index });
+
+  return Snapshot(listValues, command);
+}
+
+export function isEmpty(prevSnapshot) {
+  const listValues = prevSnapshot.get('listValues');
+
+  const command = Map({ method: 'isEmpty', arguments: [], returned: listValues.size === 0 });
+
   return Snapshot(listValues, command);
 }
--- src/models/Snapshot.test.js
+++ src/models/Snapshot.test.js
@@ -1,5 +1,5 @@
 import { List, Map } from 'immutable';
-import { Snapshot, add, get, set, remove, size } from './Snapshot';
+import { Snapshot, add, get, set, remove, size, clear, contains, indexOf, isEmpty, lastIndexOf } from './Snapshot';
 
 describe('Snapshot', () => {
   describe('add to list with items', () => {
@@ -118,7 +118,7 @@ describe('Snapshot', () => {
     });
   });
 
-  describe('remove value from list', () => {
+  describe('remove value at index from list', () => {
     let prevSnapshot;
 
     beforeEach(() => {
@@ -139,16 +139,192 @@ describe('Snapshot', () => {
     });
   });
 
+  describe('remove first occurrence of value if found', () => {
+    let prevSnapshot;
+
+    beforeEach(() => {
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' }),
+        Map({ value: 'A', id: 'A2' }),
+        Map({ value: 'C', id: 'C1' })
+      ]), Map({ method: '', arguments: [], returned: null }));
+    });
+
+    it('should remove the first occurrence of a value that is present in the list and return true', () => {
+      const newSnapshot = remove(prevSnapshot, ['A']);
+      expect(newSnapshot.get('command').get('returned')).toEqual(true);
+
+      const newListValues = newSnapshot.get('listValues');
+      expect(newListValues.size).toBe(prevSnapshot.get('listValues').size - 1);
+      expect(newListValues.get(0).get('value')).toEqual('B');
+    });
+
+    it('should not remove anything when value is not present, should return false', () => {
+      const newSnapshot = remove(prevSnapshot, ['E']);
+      expect(newSnapshot.get('command').get('returned')).toEqual(false);
+
+      const newListValues = newSnapshot.get('listValues');
+      expect(newListValues.size).toBe(prevSnapshot.get('listValues').size);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+  });
+
   describe('get size of list', () => {
     let prevSnapshot; 
 
     beforeEach(() => {
-      prevSnapshot = Snapshot(List(['A', 'B', 'C']), Map({ method: '', arguments: [], returned: null }));
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' }),
+        Map({ value: 'C', id: 'C1' })
+      ]), Map({ method: '', arguments: [], returned: null }));
     });
 
     it('should fetch the correct value from the list and not modify list values', () => {
       const newSnapshot = size(prevSnapshot, []);
       expect(newSnapshot.get('command').get('returned')).toBe(3);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+  });
+
+  describe('clear list', () => {
+    let prevSnapshot; 
+
+    beforeEach(() => {
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' }),
+        Map({ value: 'C', id: 'C1' })
+      ]), Map({ method: '', arguments: [], returned: null }));
+    });
+
+    it('should clear out all values', () => {
+      const newSnapshot = clear(prevSnapshot);
+      expect(newSnapshot.get('command').get('returned')).toBe(null);
+      expect(newSnapshot.get('listValues').size).toBe(0);
+    });
+  });
+
+  describe('check if list contains a specific value', () => {
+    let prevSnapshot; 
+
+    beforeEach(() => {
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' }),
+        Map({ value: 'C', id: 'C1' })
+      ]), Map({ method: '', arguments: [], returned: null }));
+    });
+
+    it('should return true when searching for a value that is in the list', () => {
+      const newSnapshot = contains(prevSnapshot, ['B']);
+      expect(newSnapshot.get('command').get('returned')).toBe(true);
+      expect(newSnapshot.get('listValues').size).toBe(3);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+
+    it('should return false when searching for a value that is not in the list', () => {
+      const newSnapshot = contains(prevSnapshot, ['D']);
+      expect(newSnapshot.get('command').get('returned')).toBe(false);
+      expect(newSnapshot.get('listValues').size).toBe(3);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+  });
+
+  describe('find index of first occurrence of value', () => {
+    let prevSnapshot; 
+
+    beforeEach(() => {
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' }),
+        Map({ value: 'A', id: 'A2' }),
+        Map({ value: 'C', id: 'C1' }),
+      ]), Map({ method: '', arguments: [], returned: null }));
+    });
+
+    it('should return the correct index of the first occurrence of a value that is in the list', () => {
+      const newSnapshot = indexOf(prevSnapshot, ['A']);
+      expect(newSnapshot.get('command').get('returned')).toBe(0);
+      expect(newSnapshot.get('listValues').size).toBe(4);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+
+    it('should return the correct index of the first occurrence of a value that is in the list', () => {
+      const newSnapshot = indexOf(prevSnapshot, ['B']);
+      expect(newSnapshot.get('command').get('returned')).toBe(1);
+      expect(newSnapshot.get('listValues').size).toBe(4);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+
+    it('should return -1 when the value that is not in the list', () => {
+      const newSnapshot = indexOf(prevSnapshot, ['E']);
+      expect(newSnapshot.get('command').get('returned')).toBe(-1);
+      expect(newSnapshot.get('listValues').size).toBe(4);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+  });
+
+  describe('find index of last occurrence of value', () => {
+    let prevSnapshot; 
+
+    beforeEach(() => {
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' }),
+        Map({ value: 'A', id: 'A2' }),
+        Map({ value: 'C', id: 'C1' }),
+      ]), Map({ method: '', arguments: [], returned: null }));
+    });
+
+    it('should return the correct index of the last occurrence of a value that is in the list', () => {
+      const newSnapshot = lastIndexOf(prevSnapshot, ['A']);
+      expect(newSnapshot.get('command').get('returned')).toBe(2);
+      expect(newSnapshot.get('listValues').size).toBe(4);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+
+    it('should return the correct index of the last occurrence of a value that is in the list', () => {
+      const newSnapshot = lastIndexOf(prevSnapshot, ['B']);
+      expect(newSnapshot.get('command').get('returned')).toBe(1);
+      expect(newSnapshot.get('listValues').size).toBe(4);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+
+    it('should return -1 when the value that is not in the list', () => {
+      const newSnapshot = indexOf(prevSnapshot, ['E']);
+      expect(newSnapshot.get('command').get('returned')).toBe(-1);
+      expect(newSnapshot.get('listValues').size).toBe(4);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+  });
+
+  describe('check if list is empty', () => {
+    it('should return true when the list is empty', () => {
+      let prevSnapshot; 
+
+      prevSnapshot = Snapshot(List(), Map({ method: '', arguments: [], returned: null }));
+
+      const newSnapshot = isEmpty(prevSnapshot);
+
+      expect(newSnapshot.get('command').get('returned')).toBe(true);
+      expect(newSnapshot.get('listValues').size).toBe(0);
+      expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
+    });
+
+    it('should return false when the list is not empty', () => {
+      let prevSnapshot; 
+
+      prevSnapshot = Snapshot(List([
+        Map({ value: 'A', id: 'A1' }),
+        Map({ value: 'B', id: 'B1' })
+      ]), Map({ method: '', arguments: [], returned: null }));
+      
+      const newSnapshot = isEmpty(prevSnapshot);
+
+      expect(newSnapshot.get('command').get('returned')).toBe(false);
+      expect(newSnapshot.get('listValues').size).toBe(2);
       expect(newSnapshot.get('listValues')).toEqual(prevSnapshot.get('listValues'));
     });
   });
