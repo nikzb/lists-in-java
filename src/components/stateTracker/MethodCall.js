f0713b0757ca2517503f8778843729cca8db57ee
Nik <NikBaltatzis@gmail.com>
2018-01-20 12:19:37 -0500

    Same as previous

--- src/components/stateTracker/MethodCall.js
+++ src/components/stateTracker/MethodCall.js
@@ -3,14 +3,14 @@ import React from 'react';
 import '../methodToolbox/MethodButton.css';
 
 function MethodCall(props) {
-  const classNames = ["MethodButton", `MethodButton--${props.methodName}`];
+  const classNames = ["MethodCall", "MethodButton", `MethodButton--${props.methodName}`];
 
   return (
-    <button 
+    <div 
       className={classNames.join(' ')}>
        {/* onClick={ () => { this.props.onClick(this.state.inputValues) }}  */}
       {props.methodName}({props.arguments.join(',')})
-    </button>
+    </div>
   );
 }
 
